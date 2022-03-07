383663eb27ff67a606bf3a17b2e57e8d70aff301
PouyaM <Pouyamohammadyirby@gmail.com>
2022-03-07 12:44:09 -0800

    Phase 1 completed

---
+++ getProcInfoTest.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+/**
+ * This is a test function for getProcInfo syscall.
+ */
+int main(void){
+    getProcInfo();
+    exit();
+}
---
+++ getTicksTest.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+/**
+ * This is a test function for getTicks syscall.
+ */
+int main(void){
+    getTicks();
+    exit();
+}
